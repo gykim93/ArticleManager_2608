cbbc244886b382d4a414260ca322da85eb7a6b37
gykim93 <gykim1993@gmail.com>
2026-08-10 18:44:27 +0900

    게시글 작성

--- ArticleManager_2026/src/Main.java
+++ ArticleManager_2026/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("== 프로그램 시작 ==");
     Scanner sc = new Scanner(System.in);
+    int lastArticleId = 0;
+
+    List<Article> articles = new ArrayList<>();
 
     while (true) {
       System.out.print("명령어 ) ");
@@ -15,8 +20,35 @@ public class Main {
         System.out.println("명령어를 입력해주세요.");
         continue;
       }
+      if (cmd.equals("article write")) {
+        System.out.println("== 게시글 작성 ==");
+        int id = lastArticleId + 1;
+        System.out.print("제목 : ");
+        String title = sc.nextLine().trim();
+
+        System.out.print("내용 : ");
+        String body = sc.nextLine().trim();
+
+        Article article = new Article(id, title, body);
+        articles.add(article);
+
+        System.out.printf("%d번 글이 작성되었습니다.\n", id);
+        lastArticleId++;
+      }
     }
     System.out.println("== 프로그램 종료 ==");
     sc.close();
+  }
+}
+
+class Article {
+  private int id;
+  private String title;
+  private String body;
+
+  public Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
   }
 }
