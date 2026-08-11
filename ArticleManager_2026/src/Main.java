d08794d5735bd1cf1621af4ba75e273a19866e6f
gykim93 <gykim1993@gmail.com>
2026-08-11 18:26:43 +0900

    게시글 목록

--- ArticleManager_2026/src/Main.java
+++ ArticleManager_2026/src/Main.java
@@ -34,6 +34,17 @@ public class Main {
 
         System.out.printf("%d번 글이 작성되었습니다.\n", id);
         lastArticleId++;
+      } else if (cmd.equals("article list")) {
+        System.out.println("== 게시물 목록 ==");
+        if (articles.size() == 0) {
+          System.out.println("게시글이 존재하지 않습니다.");
+        } else {
+          System.out.println("  번호  /  제목  /  내용  ");
+          for (int i = articles.size() - 1; i >= 0; i--) {
+            Article article = articles.get(i);
+            System.out.printf("  %d  /  %s  /  %s  \n", article.getId(), article.getTitle(), article.getBody());
+          }
+        }
       }
     }
     System.out.println("== 프로그램 종료 ==");
@@ -45,6 +56,18 @@ class Article {
   private int id;
   private String title;
   private String body;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getBody() {
+    return body;
+  }
 
   public Article(int id, String title, String body) {
     this.id = id;
